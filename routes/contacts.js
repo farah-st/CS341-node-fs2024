322be87dfd2307d899d8bdd40ff52ce88f83c5f4
farah-st <121980207+farah-st@users.noreply.github.com>
2024-05-04 15:51:06 -0600

    Testing changes

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,11 +2,11 @@
 
 const express = require('express');
 const router = express.Router();
-
 const contactsController = require('../controllers/contacts');
 
+// Route to retrieve all contacts
 router.get('/', contactsController.getAll);
 
+// Route to retrieve a single contact by ID
 router.get('/:id', contactsController.getSingle);
-
 module.exports = router;
