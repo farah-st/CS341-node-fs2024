d0e27c1f54ae127323fe751db335b3ea15e4903d
farah-st <121980207+farah-st@users.noreply.github.com>
2024-05-18 15:40:13 -0600

    adding try and catch

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,64 +1,83 @@
 const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
-const getContactFromBody = (body) => ({
-  firstName: body.firstName,
-  lastName: body.lastName,
-  email: body.email,
-  favoriteColor: body.favoriteColor,
-  birthday: body.birthday
-});
-
 const getAll = async (req, res) => {
-  const result = await mongodb.getDb().db().collection('contacts').find();
-  result.toArray().then((lists) => {
+  try {
+    const result = await mongodb.getDb().db().collection('contacts').find().toArray();
     res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists);
-  });
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred while fetching contacts.' });
+  }
 };
 
 const getSingle = async (req, res) => {
-  const userId = new ObjectId(req.params.id);
-  const result = await mongodb.getDb().db().collection('contacts').find({ _id: userId });
-  result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists[0]);
-  });
+  try {
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb.getDb().db().collection('contacts').findOne({ _id: userId });
+    if (result) {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(result);
+    } else {
+      res.status(404).json({ error: 'Contact not found.' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred while fetching the contact.' });
+  }
 };
 
 const createContact = async (req, res) => {
-  const contact = getContactFromBody(req.body);
-  const response = await mongodb.getDb().db().collection('contacts').insertOne(contact);
-  if (response.acknowledged) {
-    res.status(201).json(response);
-  } else {
-    res.status(500).json(response.error || 'Some error occurred while creating the contact.');
+  try {
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+    };
+    const response = await mongodb.getDb().db().collection('contacts').insertOne(contact);
+    if (response.acknowledged) {
+      res.status(201).json(response);
+    } else {
+      res.status(500).json({ error: 'Failed to create contact.' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred while creating the contact.' });
   }
 };
 
 const updateContact = async (req, res) => {
-  const userId = new ObjectId(req.params.id);
-  const contact = getContactFromBody(req.body);
-  const response = await mongodb.getDb().db().collection('contacts').replaceOne({ _id: userId }, contact);
-  
-  if (response.modifiedCount > 0) {
-    res.status(204).send();
-  } else {
-    res.status(500).json(response.error || 'Some error occurred while updating the contact.');
+  try {
+    const userId = new ObjectId(req.params.id);
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+    };
+    const response = await mongodb.getDb().db().collection('contacts').replaceOne({ _id: userId }, contact);
+    if (response.modifiedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(500).json({ error: 'Failed to update contact or no changes made.' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred while updating the contact.' });
   }
 };
 
 const deleteContact = async (req, res) => {
-  const userId = new ObjectId(req.params.id);
   try {
+    const userId = new ObjectId(req.params.id);
     const response = await mongodb.getDb().db().collection('contacts').deleteOne({ _id: userId });
     if (response.deletedCount > 0) {
-      res.status(200).send();
+      res.status(204).send();
     } else {
-      res.status(404).json({ error: 'Contact not found' });
+      res.status(404).json({ error: 'Contact not found.' });
     }
   } catch (error) {
-    res.status(500).json({ error: 'Some error occurred while deleting the contact.' });
+    res.status(500).json({ error: 'An error occurred while deleting the contact.' });
   }
 };
 
@@ -69,6 +88,3 @@ module.exports = {
   updateContact,
   deleteContact
 };
-
-
-
